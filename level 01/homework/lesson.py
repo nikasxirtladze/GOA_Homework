5a6cb08e5ef86351f2d685e9a73d3fa8088b9f96
nikasxirtladze <tornikesxirtladze@gmail.com>
2024-06-24 15:57:33 +0400

    first commit

---
+++ level 01/homework/lesson.py
@@ -0,0 +1,33 @@
+name ="nika"
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნლელობის მიმნიჭებელი სიმბოლო
+#"nika" არის ცვლადისთვის მნიშვნელობა
+
+
+surname ="sxirtladze"
+
+print(name)
+#პრინტ ფუნქცია გადაეცემა ეკრანზიე გადმოსატანი ობიექტი
+
+
+name ="nika" #ეს არის str (sring) ტიპის ცვლადი 
+age = "9" #ეს არის inr ( INTEGER )მთელი რივცხვი
+height = 156.6 #ეს არის float ტიპის ცვლადი ( ათიწილადი )
+#boolear (bool) ტიპის ცვლადი
+
+knows_programming = True
+is_ugly = False
+
+
+
+print(name + " " + surname)
+print(name +age)
+
+print(type(age))
+print(type(name))
+print(type(height))
+print(type(surname))
+print(type(knows_programming)) 
+
+
+print(name + " " + str(age))
---
+++ level 03/homework/lesson.py
@@ -0,0 +1,6 @@
+name = "nika"
+surname = "skhirtladze"
+age = 9
+hobby = "playing football"
+city = "Tbilisi"
+print(name, surname, age, hobby, city)
